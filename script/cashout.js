02ecae9468bf3f7e7d6b40be5e9753d6bab7e9c5
WT_FALL-25-26- <moonsaha721@gmail.com>
2026-02-21 15:51:33 +0600

    js started for cashout form

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById("cashout-btn").addEventListener("click", funtion(){
+    //1. get the agent number and validate
+    //2. get the amount, validate , convert to number
+    //3. get the current balcnce, validate , convert to number4
+    //4. calculate new balance
+    //5. get the pin and verify
+    //5-1  true hole show alert and set new blanace
+    //5-2 show error alert and return
+})
