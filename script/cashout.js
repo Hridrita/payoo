3e1b60b79b8f9e876ead8c49bbe5082227d4485b
WT_FALL-25-26- <moonsaha721@gmail.com>
2026-02-21 18:48:08 +0600

    added some js logic in cashout form

--- script/cashout.js
+++ script/cashout.js
@@ -4,6 +4,11 @@ const cashoutNumberInput = document.getElementById("cashout-number");
 const cashoutNumber = cashoutNumberInput.value ;
 console.log(cashoutNumber);
 
+if (cashoutNumber.length != 11){
+    alert("Inavlid Agent Number");
+    return;
+}
+
     //2. get the amount, validate , convert to number
     const cashoutAmountInput = document.getElementById("cashout-amount");
     const cashoutAmount = cashoutAmountInput.value ;
@@ -17,12 +22,26 @@ console.log(cashoutNumber);
     //4. calculate new balance
     const newBalance = Number(balance) - Number(cashoutAmount);
     
-
     if (newBalance < 0){
         alert("Invalid Amount");
     }
     console.log("New Balance", newBalance);
+
     //5. get the pin and verify
-    //5-1  true hole show alert and set new blanace
-    //5-2 show error alert and return
+    const cashoutPinInput = document.getElementById("cashout-pin");
+    const pin = cashoutPinInput.value ;
+
+    if (pin === '1234'){
+        //5-1  true hole show alert and set new blanace
+        alert("Cashout Successful");
+        console.log("New Balance", newBalance);
+        balanceElement.innerText = newBalance;
+    }
+    else{
+         //5-2 false hole show error alert and return
+        alert("Invalid Pin");
+        return;
+    }
+    
+    
 })
